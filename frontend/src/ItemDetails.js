9a9741950dc6d351766da8d1ac4bb07c11a24b85
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-04 10:21:58 -0800

    post

--- frontend/src/ItemDetails.js
+++ frontend/src/ItemDetails.js
@@ -8,24 +8,15 @@ export const ItemDetails = (props) => {
   const [inventoryList, setInventoryList] = useState([]) 
   const [pageReload, setPageReload] = useState(null) 
 
-  // useEffect(() => {
-  //   fetch(`http://localhost:8080/${itemID}`)
-  //   .then(res => res.json())
-  //   .then(data => setInventoryList(data))
-  //   }, [pageReload])
 
   return (
     <>
      <p>Item: {item.item}</p>
      <p>Description: {item.description}</p>
      <p>Quantity: {item.quantity}</p>
-    <ul>
-      {/* {inventoryList.map((item, index) => {
-        return (
-          <li key={index}>Item: {item.item} </li>
-      )
-      })} */}
-    </ul>
+
+     <button type="button">edit</button>
     </>
+
   )
 }
--- frontend/src/UserInventory.js
+++ frontend/src/UserInventory.js
@@ -6,21 +6,61 @@ export const UserInventory = () => {
   const [pageReload, setPageReload] = useState(null) 
   const [userID, setUserID] = useState(1) // SET USER ID BASED ON LOG IN
 
+  const [item, setItem] = useState('')
+  const [quantity, setQuantity] = useState(0) 
+  const [description, setDescription] = useState('') 
   useEffect(() => {
     fetch(`http://localhost:8080/items/user/${userID}`)
     .then(res => res.json())
     .then(data => setUserInventory(data))
     }, [pageReload])
 
+    const handleSubmit = () => {
+      let newItem = {
+        "item" : item,
+        "description" : description,
+        "quantity" : quantity,
+        "user_id" : userID
+      }
+    
+      fetch("http://localhost:8080/items",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      })
+        .then(setUserInventory((prev) => [...prev, newItem]))
+    }
+
+    
+    // const addItem = () => {
+    //   fetch('http://localhost:8081/items'),{
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //       item: newItem
+    //     }),
+    //     headers: {
+    //       'Content-type': 'application/json; charset=UTF-8',
+    //     },
+    //     }
+
   return (
     <>
+  
+      <input type="text" placeholder="new item..." value={item} onChange={(e)=>{setItem(e.target.value)}}></input>
+      <input type="text" placeholder="description..." value={description} onChange={(e)=>setDescription(e.target.value)}></input>
+      <input type="number" placeholder="quantity" value={quantity} onChange={(e)=>setQuantity(e.target.value)}></input>
+      <button type="submit" onClick={handleSubmit}>add item</button>
+
     <ul>
       {userInventory.map((item, index) => {
         return (
-          <>
-          <Link key={index} to="/item/detail/" state={{item}}>{item.item}</Link>
-          <br></br>
-          </>
+          
+          <li key={index}><Link to="/item/detail/" state={{item}}>{item.item}</Link></li>
+      
       )
       })}
     </ul>
